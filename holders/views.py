1e50162bb21fbb5b2175b00252cab06a5a58deb7
mugoh <mugoh.ks@gmail.com>
2019-03-23 16:24:58 +0300

    Add views for the api models

--- holders/views.py
+++ holders/views.py
@@ -2,5 +2,25 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+
+from dholders.serializers import MealSerializer, UserSerializer, DishTypeSerializer
+from holders.models import Meal, DishType
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+
+class DishTypeViewSet(viewsets.ModelViewSet):
+    queryset = DishType.objects.all()
+    serializer_class = DishTypeSerializer
